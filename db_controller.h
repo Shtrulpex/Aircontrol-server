a0d0bf9035605279120749a9c8ea136e332c6c6f
Vsevolod Fedorov <fedorov.va@phystech.edu>
2023-05-02 19:18:17 +0300

    feature(data-exchange) 	*add files [db_controller.h db_controller.cpp] 	*add declaration functions [run_query] 	*modified CMakeLists.txt

---
+++ db_controller.h
@@ -0,0 +1,13 @@
+#ifndef DB_CONTROLLER_H
+#define DB_CONTROLLER_H
+
+#include "data_object.h"
+#include "query.h"
+#include <vector>
+
+
+std::vector<Plane> run_query(PlaneQuery);
+std::vector<Airport> run_query(AirportQuery);
+
+
+#endif // DB_CONTROLLER_H
