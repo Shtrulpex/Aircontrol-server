12fad8463eedb40b7e422dfe254d39505c9019a0
Razakys <oshmetkov.ss@phystech.edu>
2023-05-12 11:46:24 +0300

    сдалан путь между аэропортами;

---
+++ flight_path.h
@@ -0,0 +1,41 @@
+#include <class.h>
+
+/* 
+        Класс Airport должен включать как минимум координаты (широта (latitude), 
+    долгота (longtitude)) в виде точки Point.
+
+        Функции angular_path_length() и path_length() определяют угловую (в радианах) 
+    и обычную (в метрах) длину пути соответственно.
+
+        Функция  latitude_function() это функция широты от долготы на заданном пути
+    (нач. и кон. точки определяют путь).
+
+        Функция  flight_path() определяет путь (вектор Point(широта, долгота)) 
+    по аэропортам start и finish при разбиении пути на count точек (включая концы).
+
+*/
+
+
+constexpr double Earth_radius{6371000};
+constexpr double delta{1000}; // разбиение пути
+
+
+
+double angular_path_length (Point, Point);
+double angular_path_length(const Airport&, const Airport&);
+
+double path_length (double angular);
+double path_length (Point, Point);
+double path_length (const std::vector<Point>&);
+double path_length(const Airport&, const Airport&);
+double path_length(const Airport&, const Airport&, const Plane&);
+
+
+std::vector<Point> flight_path (const Airport&, const Airport&); // without high
+std::vector<Point> flight_path (const Airport&, const Airport&, const Plane&);  // with high //не реализации
+
+
+bool is_current_plane(const Airport&, const Airport&, const Plane&); // with high
+
+
+double high(Point); // не реализации
